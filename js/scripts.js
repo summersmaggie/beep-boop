dd250d9794676c9d8fb57280f41097186c97b273
Maggie Summers <summers.maggie@gmail.com>
2018-01-19 10:29:56 -0800

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-//front end logic
+//biz logic
 
 
 
 
 
-//back end logic
+//ui logic
 $(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
 
+    var number = $("input#number").val();
+    var result = newNumber(number);
+
+    $("result").text(result);
+    $("#formOne").trigger("reset");
+  });
 });
