0306882436d8a775cf1d6765d7b43459be636215
Maggie Summers <summers.maggie@gmail.com>
2018-01-19 16:12:04 -0800

    clean up code for final draft

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,12 @@
 var divisibleThree = "I'm sorry, Dave. I'm afraid I can't do that.";
 var one = "Boop!";
 var zero = "Beep!";
+var resultArray = [];
 
-//returns array but no output
 var numberRange = function(number) {
-  var resultArray = [];
-  for (var i = 0; i < number; i += 1) {
+  for (var i = 1; i <= number; i += 1) {
     //array.push(i);
-  if((i % 3) === 0) {
+  if ((i % 3) === 0) {
     resultArray.push(divisibleThree);
   } else if (i === (1)) {
     resultArray.push(one);
@@ -21,32 +20,6 @@ var numberRange = function(number) {
 }
   return resultArray;
 };
-
-
-
-/*/var divisibleThree = function(number) {
-  for (var index = 0; index < number; index += 1) {
-    array.push(index);
-  }
-  if (array.includes(number % 3 === 0)) {
-    return ("I'm sorry, Dave. I'm afraid I can't do that.");
-  } else {
-    return array;
-  }
-};/*/
-
-
-//returns output but no array
-/*/var numberOutput = function(number) {
-  if (number % 3 === 0) {
-    return divisibleThree;
-  } else if (number.includes(1)) {
-    return one;
-  } else if (number.includes(0)) {
-    return zero;
-  }
-}; /*/
-
 
 //ui logic
 $(document).ready(function() {
