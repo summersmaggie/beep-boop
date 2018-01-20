b9872ce7f37ea63c51504e5404607154da37f425
Maggie Summers <summers.maggie@gmail.com>
2018-01-19 16:03:35 -0800

    integrate looping with conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,51 @@
 //biz logic
 
-var array = [];
+var divisibleThree = "I'm sorry, Dave. I'm afraid I can't do that.";
+var one = "Boop!";
+var zero = "Beep!";
 
+//returns array but no output
 var numberRange = function(number) {
-  for (var index = 0; index < number; index += 1) {
-    array.push(index);
+  var resultArray = [];
+  for (var i = 0; i < number; i += 1) {
+    //array.push(i);
+  if((i % 3) === 0) {
+    resultArray.push(divisibleThree);
+  } else if (i === (1)) {
+    resultArray.push(one);
+  } else if (i === (0)) {
+    resultArray.push(zero);
+  } else {
+    resultArray.push(i);
   }
-  return array;
+}
+  return resultArray;
 };
 
 
-/*/var numberOutput = function(number) {
-  if (number % 3 === 0) {
-    var divisibleThree = "I'm sorry, Dave. I'm afraid I can't do that.";
-    return divisibleThree;
-  } else if (number.includes(1)) {
-    var numberOne = "boop";
-    return numberOne;
-  } else if (number.includes(0)) {
-    var numberZero = "beep";
-    return numberZero;
+
+/*/var divisibleThree = function(number) {
+  for (var index = 0; index < number; index += 1) {
+    array.push(index);
+  }
+  if (array.includes(number % 3 === 0)) {
+    return ("I'm sorry, Dave. I'm afraid I can't do that.");
   } else {
-    return ("can't compute yet")
+    return array;
   }
 };/*/
+
+
+//returns output but no array
+/*/var numberOutput = function(number) {
+  if (number % 3 === 0) {
+    return divisibleThree;
+  } else if (number.includes(1)) {
+    return one;
+  } else if (number.includes(0)) {
+    return zero;
+  }
+}; /*/
 
 
 //ui logic
